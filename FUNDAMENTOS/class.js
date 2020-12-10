435aeff06ae210e16c9a52ae713cb6499022b013
Christopher Mejia <chis.mejia.garcia@gmail.com>
2020-12-09 20:08:07 -0600

    Herencia de Clases por prototipo

--- FUNDAMENTOS/class.js
+++ FUNDAMENTOS/class.js
@@ -1,3 +1,10 @@
+function heredaDe(prototipoHijo, prototipoPadre){
+    var fn = function(){}
+    fn.prototype = prototipoPadre.prototype
+    prototipoHijo.prototype = new fn
+    prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre, apellido, altura){
     this.nombre = nombre
     this.apellido = apellido
@@ -10,6 +17,19 @@ Persona.prototype.saludar = function (){
 Persona.prototype.soyAlto = function(){
     this.altura > 1.80 ? console.log(`Soy alto`) : console.log(`Soy bajito`)
 }
-var sacha = new Persona('Sacha', 'Mejia', 1.60)
-var erika = new Persona('Erika', 'Martinez', 1.84)
-var yvonne = new Persona('yvonne', 'Alonso', 1.64)
+
+function Desarrollador(nombre, apellido){
+    this.nombre = nombre
+    this.apellido = apellido
+}
+
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function () {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
+}
+
+
+// var sacha = new Persona('Sacha', 'Mejia', 1.60)
+// var erika = new Persona('Erika', 'Martinez', 1.84)
+// var yvonne = new Persona('yvonne', 'Alonso', 1.64)
