91f963cddf0bfff66dd53c4e0dd60735ceba3ad4
Christopher Mejia <chis.mejia.garcia@gmail.com>
2020-12-15 19:59:55 -0600

    Multiples peticiones de modo asincrono

--- FUNDAMENTOS/callbacks.js
+++ FUNDAMENTOS/callbacks.js
@@ -1,8 +1,21 @@
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
-const LUKE_URL = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const opts = {crossDomain: true}
 
-$.get(LUKE_URL, opts, function(luke){
-    console.log(`Hola yo soy ${luke.name}`)
-});
+//Arrow Function
+const onPeopleResponse = (persona) => {
+    console.log(`Hola, yo soy- ${persona.name}`)
+}
+
+function getPerson(id){
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, opts, onPeopleResponse )
+}
+
+getPerson(1)
+getPerson(2)
+getPerson(3)
+getPerson(4)
+getPerson(5)
+
+
