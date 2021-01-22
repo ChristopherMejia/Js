942078b120323ff99e7499081f3055c5270009ff
Christopher Mejia <chis.mejia.garcia@gmail.com>
2021-01-21 18:48:25 -0600

    examples with new features from ecma

--- ecmascript/src/es6/index.js
+++ ecmascript/src/es6/index.js
@@ -78,7 +78,77 @@ let listOfNames = name.map(function (item){
     console.log(item.name);
 })
 
-console.log(listOfNames)
+console.log(listOfNames);
+
+//Promise
+
+const helloPromise = () => {
+    return new Promise ((resolve, reject) => {
+        if(true){
+            resolve('HEY!')
+        }else {
+            reject('Ups!!')
+        }
+    });
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .then(() => console.log('hola'))
+    .catch(error => console.log(error))
 
 
- 
+///Clases en Js ecma6
+
+class calculator {
+    constructor(){
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+
+    suma(valueA, valueB){
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.suma(2,2));
+
+//import logic since other pagine
+import {hello} from './module.js';
+
+console.log(hello())
+
+//exec
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+const match = regexData.exec('2018-04-20');
+
+const year = match[1]
+const month = match[2]
+const day = match[3]
+
+console.log(year, month, day)
+
+//flat, nos permite aplanar arreglos
+let array = [1,2,3, [1,2,3,[1,2,3]]];
+console.log(array.flat(2))
+
+//flat.map permite apalnar arreglos pero antes podemos modificarlos
+let array = [1,2,3,4,5];
+console.log(array.flatMap(value => [value, value * 2]));
+
+//Elmina espacios de un string
+let hello ="           hello world"
+console.log(hello.trimStart())
+
+let hello = "hello world            "
+console.log(hello.trimEnd())
+
+//Creamos un objeto desde un arreglo o viseversa
+let entries = [["name", "oscar"], ["age", 32]];
+console.log(Object.fromEntries(entries))
+
+
+
